d4b1d62db712418e3047d3c8a6f4d55ec169729b
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-02 19:27:11 -0600

    light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 // components
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import Todos from './components/Todos/Todos';
 
 function App() {
+  const [scheme, setScheme] = useState<'light' | 'dark'>('light');
+
   return (
-    <div className="main light-scheme">
-      <Header />
+    <div className={`main ${scheme}-scheme`}>
+      <Header scheme={scheme} setScheme={setScheme} />
       <div className="main__box">
         <Form />
         <Todos />
