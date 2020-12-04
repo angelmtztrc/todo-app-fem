8b403a49d74eb5238323efcb8b453e2520ec1bda
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-03 18:50:23 -0600

    some styles details

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -20,36 +20,41 @@ const Todos = ({ todos, setTodos }: TodosProps) => {
   };
 
   return (
-    <div className="todos__container">
-      <ul className="todos__list">
-        {filter === 'completed'
-          ? todos
-              .filter(todo => todo.completed)
-              .map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
-          : filter === 'active'
-          ? todos
-              .filter(todo => !todo.completed)
-              .map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
-          : todos.map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)}
-      </ul>
-      <footer className="todos__footer">
-        <p>{todos.filter(todo => !todo.completed).length} items left</p>
-        <div className="todos__filter">
-          <p className={filter === 'all' ? 'active' : ''} onClick={() => setFilter('all')}>
-            All
-          </p>
-          <p className={filter === 'active' ? 'active' : ''} onClick={() => setFilter('active')}>
-            Active
-          </p>
-          <p
-            className={filter === 'completed' ? 'active' : ''}
-            onClick={() => setFilter('completed')}
-          >
-            Completed
-          </p>
-        </div>
-        <p onClick={handleClear}>Clear Completed</p>
-      </footer>
+    <div className="todos">
+      <div className="todos__container">
+        <ul className="todos__list">
+          {filter === 'completed'
+            ? todos
+                .filter(todo => todo.completed)
+                .map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
+            : filter === 'active'
+            ? todos
+                .filter(todo => !todo.completed)
+                .map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
+            : todos.map(todo => (
+                <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
+              ))}
+        </ul>
+        <footer className="todos__footer">
+          <p>{todos.filter(todo => !todo.completed).length} items left</p>
+          <div className="todos__filter">
+            <p className={filter === 'all' ? 'active' : ''} onClick={() => setFilter('all')}>
+              All
+            </p>
+            <p className={filter === 'active' ? 'active' : ''} onClick={() => setFilter('active')}>
+              Active
+            </p>
+            <p
+              className={filter === 'completed' ? 'active' : ''}
+              onClick={() => setFilter('completed')}
+            >
+              Completed
+            </p>
+          </div>
+          <p onClick={handleClear}>Clear Completed</p>
+        </footer>
+      </div>
+      <p className="todos__dialog">Drag and drop to reorder list</p>
     </div>
   );
 };
